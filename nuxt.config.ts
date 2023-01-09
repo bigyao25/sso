fab8e76d26028980e795d19cb1cbcc2b30f70694
dayao <rui.yao@ascendex.io>
2023-01-09 18:11:11 +0800

    .

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,4 +2,12 @@
 export default defineNuxtConfig({
   modules: [["nuxt-vue3-google-signin", { clientId: "389924773294-s7t01ql7g2jhd83lbo80d6t6inqfn1e1.apps.googleusercontent.com" }]],
   googleSignIn: { clientId: "389924773294-s7t01ql7g2jhd83lbo80d6t6inqfn1e1.apps.googleusercontent.com" },
+  runtimeConfig: {
+    sso: {
+      google: {
+        clientSecret: "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",
+        clientId: "389924773294-s7t01ql7g2jhd83lbo80d6t6inqfn1e1.apps.googleusercontent.com",
+      },
+    },
+  },
 });
--- server/api/google/callback.post.ts
+++ server/api/google/callback.post.ts
@@ -1,21 +1,21 @@
 import { OAuth2Client } from "google-auth-library";
 
-const CLIENT_ID = "389924773294-s7t01ql7g2jhd83lbo80d6t6inqfn1e1.apps.googleusercontent.com";
-const CLIENT_SECRET = "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";
 // const REDIRECT_URL = "http://localhost/dashboard";
 // const REDIRECT_URL = "http://localhost:3000/dashboard";
 const REDIRECT_URL = "postmessage";
 // const REDIRECT_URL = "https://ascendex.com/en/global-digital-asset-platform";
 
 function getOauth2Client() {
-  const oAuth2Client = new OAuth2Client(CLIENT_ID, CLIENT_SECRET, REDIRECT_URL);
+  const { sso } = useRuntimeConfig();
+  const {
+    google: { clientId, clientSecret },
+  } = sso;
+  const oAuth2Client = new OAuth2Client(clientId, clientSecret, REDIRECT_URL);
 
   return oAuth2Client;
 }
 
 export default defineEventHandler(async event => {
-  //   const client = new OAuth2Client(CLIENT_ID);
-
   try {
     // code comes in the body
     const { code } = await readBody(event);
