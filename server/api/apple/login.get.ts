0fcd1ed4af9404e02ac944e65dd209989ab101f3
dayao <rui.yao@ascendex.io>
2023-01-10 16:04:16 +0800

    .

--- server/api/apple/login.get.ts
+++ server/api/apple/login.get.ts
@@ -2,7 +2,7 @@ import appleSignin from "apple-signin-auth";
 
 const options = {
   clientID: "com.ascendex.ssotest", // Apple Client ID
-  redirectUri: "https://yaorui.test105.ascendex-sandbox.com/apple/callback",
+  redirectUri: "https://yaorui.test105.ascendex-sandbox.com/api/apple/callback",
   // OPTIONAL
   state: "state", // optional, An unguessable random string. It is primarily used to protect against CSRF attacks.
   responseMode: "fragment" as "query" | "fragment" | "form_post", // Force set to form_post if scope includes 'email'
