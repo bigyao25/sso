2b28c29c7ac26b1a9e79b2aca89cc0e1730350c4
dayao <rui.yao@ascendex.io>
2023-01-13 11:19:12 +0800

    .

--- server/api/apple/token.post.ts
+++ server/api/apple/token.post.ts
@@ -12,7 +12,7 @@ export default defineEventHandler(async event => {
   event.node.res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   event.node.res.setHeader("Access-Control-Allow-Credentials", "true");
 
-  event.node.res.setHeader("Set-Cookie", `token=${body.id_token}; Secure; HttpOnly`);
+  event.node.res.setHeader("Set-Cookie", `token=${body.id_token};`);
   event.node.res.setHeader("Location", "https://yaorui.test105.ascendex-sandbox.com/apple2/ok");
   event.node.res.statusCode = 302;
   event.node.res.end();
